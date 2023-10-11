2d53de33ef8498300b70f344c26e65c167496023
Mayon-Francis <mayonfrancis@gmail.com>
2023-10-11 13:59:31 +0530

    Loader Launch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,75 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Footer from "@/components/Footer";
-import Home1 from "@/components/Home";
-import Getstarted from "@/components/Getstarted";
-import Navbar from "@/components/Navbar";
-import Aos from "aos";
-import { useEffect } from "react";
-import Roadmap from "@/components/Roadmap";
-import Timeline from "@/components/Timeline";
-import Sponsors from "@/components/Sponsors";
-import Contact from "@/components/Contact";
-import Faq from "@/components/Faq";
-import Rewards from "@/components/Rewards";
-import Problem_stmnt from "@/components/Problem_stmtn";
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Footer from '@/components/Footer';
+import Home1 from '@/components/Home';
+import Getstarted from '@/components/Getstarted';
+import Navbar from '@/components/Navbar';
+import Aos from 'aos';
+import { useEffect, useState } from 'react';
+import Roadmap from '@/components/Roadmap';
+import Timeline from '@/components/Timeline';
+import Sponsors from '@/components/Sponsors';
+import Contact from '@/components/Contact';
+import Faq from '@/components/Faq';
+import Rewards from '@/components/Rewards';
+import Problem_stmnt from '@/components/Problem_stmtn';
+import axios from 'axios';
+import Loader from '@/components/Loader';
+import Head from 'next/head';
 
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+	const launchurl = `${process.env.NEXT_PUBLIC_LAUNCH_BACKEND_URL}/launch/status`;
 
-    useEffect(() => {
-      Aos.init({
-        offset: 50,
-      });
-    }, []);
-      
-  return (
-    <>
-      <title>IBeTo | Excel 2023</title>
-      <Navbar />
-      <div className="fixed">
-        <Home1 />
-      </div>
-      <div id="start" className="relative top-[90vh] sm:top-[88vh]">
-        <Getstarted />
-        <Roadmap />
-        <Problem_stmnt />
-        <Timeline />
-        <Rewards/>
-        <Sponsors />
-        <div className="absolute w-full z-50">
-          <Faq />
-          <Contact />
-          <Footer />
-          
-        </div>
-        
-      </div>
-    </>
-  );
+	useEffect(() => {
+		Aos.init({
+			offset: 50,
+		});
+	}, []);
+
+	const [isLaunched, setLaunched] = useState(false);
+	useEffect(() => {
+		if (!isLaunched) {
+			const fetchData = async () => {
+				try {
+					const res = await axios.get(launchurl);
+					setLaunched(res.data.launchStatus.websiteStatus);
+				} catch (error) {
+					console.log(error);
+				}
+			};
+			const intervalId = setInterval(fetchData, 2000);
+			return () => clearInterval(intervalId);
+		}
+	}, [isLaunched]);
+
+	return isLaunched ? (
+		<>
+			<Head>
+				<title>IBeTo | Excel 2023</title>
+				<link rel='icon' href='/ibetologowithouttext.png' as='image' />
+			</Head>
+
+			<Navbar />
+			<div className='fixed'>
+				<Home1 />
+			</div>
+			<div id='start' className='relative top-[90vh] sm:top-[88vh]'>
+				<Getstarted />
+				<Roadmap />
+				<Problem_stmnt />
+				<Timeline />
+				<Rewards />
+				<Sponsors />
+				<div className='absolute w-full z-50'>
+					<Faq />
+					<Contact />
+					<Footer />
+				</div>
+			</div>
+		</>
+	) : (
+		<Loader />
+	);
 }
